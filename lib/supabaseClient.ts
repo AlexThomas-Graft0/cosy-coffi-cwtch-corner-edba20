09a0eecdee9b8e52ee543e10a870724ea501906c
Alex Thomas <48531362+AlexThomas-Graft0@users.noreply.github.com>
2026-07-04 18:33:56 +0100

    add lib/supabaseClient.ts

---
+++ lib/supabaseClient.ts
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+  { db: { schema: "cosy_coffi_cwtch_corner_edba20" } }
+);
